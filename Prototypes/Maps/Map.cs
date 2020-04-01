9297a76e00330fed9d05c2d92db0e2c716f81e5d
1513_IRON\1513 IRON <gabrielfritter@gmail.com>
2020-03-31 21:29:18 -0300

    Commit with the final pre-prototype build.

--- Prototypes/Maps/Map.cs
+++ Prototypes/Maps/Map.cs
@@ -53,6 +53,11 @@ public class Map : Node2D
 
     }
 
+    public void OnBaseDied()
+    {
+        EmitSignal("OnBaseDeath");
+    }
+
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
 //  {
