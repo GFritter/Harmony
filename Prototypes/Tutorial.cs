1e7e34612ff474af0fa620b3c80e91cebd33fd0d
ErickCBorges <erickcandidoborges@gmail.com>
2020-06-06 15:53:00 -0300

    Adicionado: mapas(alguns já completos outros em andamento) e tutorial.

--- Prototypes/Tutorial.cs
+++ Prototypes/Tutorial.cs
@@ -5,6 +5,8 @@ public class Tutorial : Control
 {
     [Export]
     public PackedScene mainScene;
+    public PackedScene prevScene;
+    public PackedScene nextScene;
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
@@ -19,6 +21,15 @@ public class Tutorial : Control
     void LoadMainScene()
     {
         GetTree().ChangeSceneTo(mainScene);
+    }
+
+    void LoadPrevScene()
+    {
+        GetTree().ChangeSceneTo(prevScene);
+    }
+    void LoadNextScene()
+    {
+        GetTree().ChangeSceneTo(nextScene);
     }
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
